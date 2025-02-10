b2cccf76383cf7f99e04f6c72ba451a6da5a3097
richard <richard@provalidator.com>
2025-02-10 17:29:02 +0900

    UI - Home, ProductList, ProductDetail

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="bg-pink-400 p-4 text-white text-center">
+      &copy; {new Date().getFullYear()} ShopEasy. All rights reserved. Design by
+      h662.
+    </footer>
+  );
+}
+
+export default Footer;
